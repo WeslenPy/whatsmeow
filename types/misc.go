89761ddb0f359e0bd5a4390606f14806c8eef823
weslenpy <weslenjhony@gmail.com>
2026-06-06 00:53:04 -0300

    new

--- types/misc.go
+++ types/misc.go
@@ -3,6 +3,7 @@ package types
 type ReachoutTimeoutLock struct {
 	IsActive            bool `json:"is_active"`
 	TimeEnforcementEnds any  `json:"time_enforcement_ends"`
+	EnforcementType     any  `json:"enforcement_type"`
 }
 
 type BizIntegrity struct {
